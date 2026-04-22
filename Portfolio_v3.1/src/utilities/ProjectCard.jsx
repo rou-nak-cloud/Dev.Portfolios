cdce2ef116094c7763c99533f53e82d069db0a53
rou-nak-cloud <github.rounak.19@gmail.com>
2026-04-22 12:29:02 +0530

    project card overflow only for mobile

--- Portfolio_v3.1/src/utilities/ProjectCard.jsx
+++ Portfolio_v3.1/src/utilities/ProjectCard.jsx
@@ -19,7 +19,7 @@ export default function ProjectCard() {
         const isLeft = i % 2 === 0;
 
         gsap.from(card, {
-          x: isLeft ? -100 : 100, // alternate direction
+          xPercent: isLeft ? -30 : 30, // alternate direction
           y: 10, // subtle upward feel
           rotate: isLeft ? -4 : 4, //  subtle tilt
           opacity: 0,
@@ -43,7 +43,7 @@ export default function ProjectCard() {
   return (
     <div
       ref={containerRef}
-      className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-3 md:gap-4 px-2 sm:px-3 md:px-5 "
+      className="max-sm:overflow-x-hidden grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-3 md:gap-4 px-2 sm:px-3 md:px-5 "
     >
       {PROJECTS.map((project, idx) => (
         <a
