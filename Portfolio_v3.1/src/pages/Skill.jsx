89b283d6810dc7d97faad4f29e3097fc2305a0b3
rou-nak-cloud <github.rounak.19@gmail.com>
2026-04-23 10:50:51 +0530

    Asthetic clock component it is very asthetic..

--- Portfolio_v3.1/src/pages/Skill.jsx
+++ Portfolio_v3.1/src/pages/Skill.jsx
@@ -1,15 +1,20 @@
+import Footer from "../components/Footer";
 import CurrentFocus from "../components/SkillPage/CurrentFocus";
 import SkillInfo from "../components/SkillPage/SkillInfo";
 import SkillSection from "../components/SkillPage/SkillSection";
 import SkillGroups from "../components/SkillPage/SkillsGroups";
+import AestheticClock from "../utilities/AstheticClock";
+import TimeFormat from "../utilities/TimeFormat";
 
 export default function Skill() {
   return (
     <section>
       <SkillSection />
+      <AestheticClock />
       <CurrentFocus />
       <SkillInfo />
       <SkillGroups />
+      <Footer />
     </section>
   );
 }
